bad03528caf89e2d3786037d13a5959d93096cb8
ryan <rschemm@godaddy.com>
2017-08-08 06:36:17 -0700

    adding in params for director_servers, director_mail_servers, director_port, lmtp_proxy, lmtp_port and lmtp_haproxy.  adding notify service dovecot whenever tempalte is updated

--- manifests/director.pp
+++ manifests/director.pp
@@ -1,7 +1,16 @@
 class dovecot::director (
+    # 10-director.conf
+    Array $director_servers      = undef,
+    Array $director_mail_servers = undef,
+    Integer $director_port       = undef,
+    $lmtp_proxy                  = no,
+    Integer $lmtp_port           = undef,
+    $lmtp_haproxy                = undef,
+
 ) {
 
     file { "${dovecot::directory}/conf.d/10-director.conf":
         content => template('dovecot/conf.d/10-director.conf.erb'),
+        notify  => Service['dovecot'];
     }
 }
