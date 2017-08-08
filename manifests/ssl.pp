41f1f3f690e7f8550abdffc0e97f4c25118a0d7d
ryan <rschemm@godaddy.com>
2017-08-08 06:47:18 -0700

    casting parameters as specific types rather than using validate_ functions from stdlib.  adding notify service dovecot whenever tempalte is updated

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -1,20 +1,15 @@
 class dovecot::ssl (
     # 10-ssl.conf
-    $ssl                          = undef,
-    $ssl_cert                     = '/etc/pki/dovecot/certs/dovecot.pem',
-    $ssl_key                      = '/etc/pki/dovecot/private/dovecot.pem',
-    $ssl_cipher_list              = undef,
-    $ssl_protocols                = undef,
-    $ssl_dh_parameters_length     = undef,
+    Boolean $ssl              = true,
+    $ssl_cert                 = undef,
+    $ssl_key                  = undef,
+    $ssl_cipher_list          = undef,
+    $ssl_protocols            = undef,
+    $ssl_dh_parameters_length = undef,
 ) {
-
-    # 10-ssl.conf
-    validate_string($ssl)
-    validate_string($ssl_cert)
-    validate_string($ssl_key)
-    validate_string($ssl_key)
 
     file { "${dovecot::directory}/conf.d/10-ssl.conf":
         content => template('dovecot/conf.d/10-ssl.conf.erb'),
+        notify  => Service['dovecot'];
     }
 }
