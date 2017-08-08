51f24b045ef6f252d91def9a38e447b669d72289
ryan <rschemm@godaddy.com>
2017-08-08 06:38:07 -0700

    making work with director setup, mainly a tcp listener, protocol_doveadm param and proxy stuffs.  adding notify service dovecot whenever tempalte is updated

--- manifests/doveadm.pp
+++ manifests/doveadm.pp
@@ -1,17 +1,16 @@
 class dovecot::doveadm (
     # 30-doveadm.conf
-    $password     = undef,
-    $port         = undef,
-    $ssl          = undef,
+    String $password  = undef,
+    $proxy_port       = undef,
+    $http_port        = undef,
+    $ssl              = undef,
+    $tcp_port         = undef,
+    $protocol_doveadm = undef,
 
 ) {
 
-    # 10-master.conf
-    validate_string($password)
-    validate_integer($port)
-
     file { "${dovecot::directory}/conf.d/30-doveadm.conf":
         content => template('dovecot/conf.d/30-doveadm.conf.erb'),
+        notify  => Service['dovecot'];
     }
-
 }
