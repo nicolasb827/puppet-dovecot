358971dddb7f8c14410e69dd4440721c9d34a3c4
ryan <rschemm@godaddy.com>
2017-08-08 06:35:04 -0700

    adding authdb and passdb params.  adding notify service dovecot whenever tempalte is updated

--- manifests/auth_passwdfile.pp
+++ manifests/auth_passwdfile.pp
@@ -1,10 +1,11 @@
 class dovecot::auth_passwdfile (
     # auth-passwdfile.conf.ext
-    $auth_passwdfile_passdb       = undef,
-    $auth_passwdfile_userdb       = undef,
+    $passdb       = undef,
+    $userdb       = undef,
 ) {
 
     file { "${dovecot::directory}/conf.d/auth-passwdfile.conf.ext" :
         content => template('dovecot/conf.d/auth-passwdfile.conf.ext.erb'),
+        notify => Service['dovecot'];
     }
 }
